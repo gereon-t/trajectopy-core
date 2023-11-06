28b643f8cd1419db8bdeb8e3d7c60d12d5110c31
Gereon Tombrink <tombrink@igg.uni-bonn.de>
2023-11-06 10:37:15 +0100

    customizable plotting style

--- trajectopy_core/plotting/__init__.py
+++ trajectopy_core/plotting/__init__.py
@@ -1,9 +1,25 @@
 import os
-
+import logging
 import matplotlib.pyplot as plt
+
+logger = logging.getLogger("root")
 
 plt.rcParams["figure.max_open_warning"] = 50
 
+
+def mplstyle_file_path() -> str:
+    custom_path = os.path.join("./custom.mplstyle")
+    if os.path.isfile(custom_path):
+        logger.info("Using custom matplotlib style from %s", custom_path)
+        return custom_path
+
+    logger.info(
+        "Using default settings for matplotlib style. You can use custom styles by creating a 'custom.mplstyle' file in the current directory."
+    )
+    return "default.mplstyle"
+
+
 base_path = os.path.join(os.path.dirname(__file__))
-MPL_STYLE_PATH = os.path.join(base_path, "default.mplstyle")
+MPL_STYLE_PATH = os.path.join(base_path, mplstyle_file_path())
 plt.style.use(MPL_STYLE_PATH)
+logger.info("Using matplotlib style: %s", MPL_STYLE_PATH)
