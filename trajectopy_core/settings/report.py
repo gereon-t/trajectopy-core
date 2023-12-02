f09d46d18b6142621023921d6fc229451ebae110
Gereon Tombrink <tombrink@igg.uni-bonn.de>
2023-12-02 19:16:25 +0100

    removed setting with no effect

--- trajectopy_core/settings/report.py
+++ trajectopy_core/settings/report.py
@@ -32,7 +32,6 @@ class ReportSettings(Settings):
     """
     ReportSettings class represents the settings for generating reports.
 
-    - `report_width` (str): The width of the report in pixels. Default value is "800px".
     - `scatter_max_std` (float): The upper colorbar limit is set to the mean plus this value times the standard deviation of the data. This is useful to prevent outliers from dominating the colorbar. Default value is 4.0.
     - `ate_unit_is_mm` (bool): Indicates whether the unit of Absolute Trajectory Error (ATE) is millimeters. Default value is False.
     - `directed_ate` (bool): Indicates whether the ATE is split into along-, horizontal-cross- and vertical-cross-track direction. Default value is True.
@@ -64,7 +63,6 @@ class ReportSettings(Settings):
     - `three_subplots_height` (int): The height of three subplots. Default value is 750.
     """
 
-    report_width: str = "800px"
     scatter_max_std: float = 4.0
     ate_unit_is_mm: bool = False
     directed_ate: bool = True
