bfebc048cb69205fcc4021cd8bbd2f8b4098b5bf
Gereon Tombrink <tombrink@igg.uni-bonn.de>
2023-12-02 20:23:08 +0100

    added approximation settings to processing settings

--- trajectopy_core/settings/processing.py
+++ trajectopy_core/settings/processing.py
@@ -8,6 +8,7 @@ mail@gtombrink.de
 from dataclasses import dataclass, field
 
 from trajectopy_core.settings.alignment import AlignmentSettings
+from trajectopy_core.settings.approximation import ApproximationSettings
 from trajectopy_core.settings.base import Settings
 from trajectopy_core.settings.comparison import RelativeComparisonSettings
 from trajectopy_core.settings.matching import MatchingSettings
@@ -21,6 +22,7 @@ class ProcessingSettings(Settings):
     alignment: AlignmentSettings = field(default_factory=AlignmentSettings)
     matching: MatchingSettings = field(default_factory=MatchingSettings)
     relative_comparison: RelativeComparisonSettings = field(default_factory=RelativeComparisonSettings)
+    approximation: ApproximationSettings = field(default_factory=ApproximationSettings)
     sorting: SortingSettings = field(default_factory=SortingSettings)
 
 
